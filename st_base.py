bd4eadd97290049fcec6bab715be8e4ff94cba95
hgctmy <hgctomo@gmail.com>
2024-01-17 16:36:12 +0900

    -

--- st_base.py
+++ st_base.py
@@ -11,11 +11,7 @@ import json
 conn = st.connection('gpt4_db', type='sql')
 with conn.session as s:
     s.execute("CREATE TABLE IF NOT EXISTS gpt4 (input TEXT, output TEXT);")
-    s.execute("INSERT INTO gpt4 (input, output) VALUES ('testinput', 'testoutput');")
-    s.execute("DELETE FROM gpt4;")
     s.commit()
-pet_owners = conn.query("select input,output from gpt4;", ttl=100)
-st.markdown(pet_owners)
 
 service_account_key = json.loads(st.secrets.GoogleKey.json_key)
 credentials = Credentials.from_service_account_info(service_account_key)
